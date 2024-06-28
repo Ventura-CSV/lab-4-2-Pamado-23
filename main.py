7d6267769d9ae22510ad27dee9c77b11e9ec16bd
Pamado-23 <pamado1@my.vcccd.edu>
2024-06-28 01:09:25 -0700

    Lab 4-2

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ def main():
     """
     total = 0
 
+    for i in range(5):
+        user_input = int(input())
+
+        total += user_input
+
+    print (f'{total}')
+
+
     ########################################
     # Do not delete the return statement
     ########################################
